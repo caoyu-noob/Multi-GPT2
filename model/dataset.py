1d491d2a3f0472f652cd910d69d4b6662480b164
CY <24397651@qq.com>
2021-08-26 09:40:56 +0800

    update data processing

--- model/dataset.py
+++ model/dataset.py
@@ -15,11 +15,9 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import json
-import math
 import os
 import random
 
-import fasttext
 import torch
 from torch.utils.data import Dataset
 from tqdm import tqdm
@@ -56,7 +54,16 @@ class FacebookDataset(Dataset):
 
                 if dialog_line[0].startswith('your persona:'):
                     persona_info = dialog_line[0].replace('your persona: ', '')
+                    if persona_info[-1] == '.' and persona_info[-2] != ' ':
+                        persona_info = persona_info[:-1] + ' .'
                     data[-1]['persona_info'].append(persona_info)
+                if dialog_line[0].startswith('partner\'s person'):
+                    if not data[-1].__contains__('partner_persona_info'):
+                        data[-1]['partner_persona_info'] = []
+                    persona_info = dialog_line[0].replace('partner\'s persona: ', '')
+                    if persona_info[-1] == '.' and persona_info[-2] != ' ':
+                        persona_info = persona_info[:-1] + ' .'
+                    data[-1]['partner_persona_info'].append(persona_info)
 
                 elif len(dialog_line) > 1:
                     data[-1]['dialog'].append(dialog_line[0])
@@ -104,21 +111,34 @@ class FacebookDataset(Dataset):
                 data[-1]['dialog'].append(items[1])
             return data
 
+
     @staticmethod
-    def make_dataset(data, vocab):
+    def make_dataset(data, vocab, only_final=False):
         dataset = []
         if isinstance(vocab, OpenAIGPTTokenizer) or isinstance(vocab, GPT2Tokenizer) or isinstance(vocab, Seq2seqTokenizer):
             for chat in tqdm(data):
                 persona_info = [vocab.encode(vocab.tokenize(s)) for s in chat['persona_info']]
 
                 dialog = []
-                for i, replica in enumerate(chat['dialog'], 1):
-                    dialog.append(vocab.encode(vocab.tokenize(replica)))
-                    if not i % 2:
-                        if chat['candidates']:
-                            candidates_ids = [vocab.encode(vocab.tokenize(c)) for c in chat['candidates'][(i - 1) // 2]]
-                            dataset.append((persona_info, dialog[:], candidates_ids))
-                        else:
+                if only_final:
+                    for utterance in chat['dialog']:
+                        dialog.append(vocab.encode(vocab.tokenize(utterance)))
+                    dataset.append((persona_info, dialog[:], []))
+                else:
+                    for i, replica in enumerate(chat['dialog'], 1):
+                        dialog.append(vocab.encode(vocab.tokenize(replica)))
+                        if not i % 2:
+                            if chat['candidates']:
+                                candidates_ids = [vocab.encode(vocab.tokenize(c)) for c in chat['candidates'][(i - 1) // 2]]
+                                dataset.append((persona_info, dialog[:], candidates_ids))
+                            else:
+                                dataset.append((persona_info, dialog[:], []))
+                if chat.__contains__('partner_persona_info'):
+                    persona_info = [vocab.encode(vocab.tokenize(s)) for s in chat['partner_persona_info']]
+                    dialog = []
+                    for i, replica in enumerate(chat['dialog'], 1):
+                        dialog.append(vocab.encode(vocab.tokenize(replica)))
+                        if i % 2 and i > 2:
                             dataset.append((persona_info, dialog[:], []))
         else:
             for chat in tqdm(data):
@@ -136,7 +156,7 @@ class FacebookDataset(Dataset):
 
         return dataset
 
-    def __init__(self, paths, vocab, *, max_lengths=2048, min_infos=2, dialog_embeddings=False,
+    def __init__(self, paths, vocab, *, max_lengths=512,  max_y_length=80, min_infos=2, dialog_embeddings=False,
                  use_start_end=True, negative_samples=0, limit_size=-1,
                  cache=None, augment=False, aug_syn_proba=0.1, aug_vary_length=True, max_history_size=-1,
                  single_input=False, data_type='persona', parsed_data=None, few_shot=False, task_map_path=None):
@@ -151,6 +171,7 @@ class FacebookDataset(Dataset):
 
         self.vocab = vocab
         self.max_lengths = max_lengths
+        self.max_y_length = max_y_length
         self.min_infos = min_infos
         self.dialog_embeddings = dialog_embeddings
         self.use_start_end = use_start_end
@@ -162,18 +183,7 @@ class FacebookDataset(Dataset):
         if cache and os.path.exists(cache):
             self.data = torch.load(cache)
         else:
-            if data_type == 'persona':
-                if not parsed_data:
-                    parsed_data = sum([FacebookDataset.parse_data(path) for path in paths], [])
-                self.data = FacebookDataset.make_dataset(parsed_data, vocab)
-            elif data_type == 'emoji':
-                if not parsed_data:
-                    parsed_data = sum([FacebookDataset.parse_data_emoji(path) for path in paths], [])
-                self.data = FacebookDataset.make_dataset(parsed_data, vocab)
-            elif data_type == 'daily':
-                if not parsed_data:
-                    parsed_data = sum([FacebookDataset.parse_data_daily(path) for path in paths], [])
-                self.data = FacebookDataset.make_dataset(parsed_data, vocab)
+            self.data = self._parse_data(paths, vocab, data_type, parsed_data)
             if cache:
                 torch.save(self.data, cache)
         if limit_size > 0:
@@ -255,31 +265,34 @@ class FacebookDataset(Dataset):
         h = []
         history_start = 0
         if self.max_history_size != -1:
-            history_start = -1 - 2 * self.max_history_size
+            history_start = -1 - self.max_history_size
+        dialog_history = dialog[history_start: -1]
         if self.single_input:
-            for i, ids in enumerate(dialog[history_start: -1]):
-                if i % 2 == 1:
+            for i, ids in enumerate(dialog_history):
+                if (len(dialog_history) - i) % 2 == 0:
                     ids = [self.vocab.talker1_bos_id] + ids
                 else:
                     ids = [self.vocab.talker2_bos_id] + ids
                 if self.dialog_embeddings:
-                    ids = [[tok, self.vocab.talker1_bos_id if i % 2 == 1 else self.vocab.talker2_bos_id] for tok in ids]
+                    ids = [[tok, self.vocab.talker1_bos_id if (len(dialog_history) - i) % 2 == 0
+                            else self.vocab.talker2_bos_id] for tok in ids]
                 h.extend(ids)
         elif not self.single_input and not self.dialog_embeddings:
-            for i, ids in enumerate(dialog[history_start: -1]):
-                if i % 2 == 1:
+            for i, ids in enumerate(dialog_history):
+                if (len(dialog_history) - i) % 2 == 0:
                     ids = [self.vocab.talker1_bos_id] + ids
                 else:
                     ids = [self.vocab.talker2_bos_id] + ids
                 h.extend(ids)
         else:
-            for i, ids in enumerate(dialog[history_start:-1]):
-                if i % 2 == 1 and self.use_start_end:
+            for i, ids in enumerate(dialog_history):
+                if (len(dialog_history) - i) % 2 == 0 and self.use_start_end:
                     ids = [self.vocab.talker1_bos_id] + ids + [self.vocab.talker1_eos_id]
                 elif self.use_start_end:
                     ids = [self.vocab.talker2_bos_id] + ids + [self.vocab.talker2_eos_id]
                 if self.dialog_embeddings:
-                    ids = [[tok, self.vocab.talker1_dialog_id if i % 2 == 1 else self.vocab.talker2_dialog_id] for tok in ids]
+                    ids = [[tok, self.vocab.talker1_dialog_id if (len(dialog_history) - i) % 2 == 0
+                            else self.vocab.talker2_dialog_id] for tok in ids]
                 h.extend(ids)
             h = h[-self.max_lengths:]
 
@@ -291,10 +304,10 @@ class FacebookDataset(Dataset):
                     y = [[tok, self.vocab.talker1_bos_id] for tok in y]
                 sentences.append(y)
             elif not self.single_input and not self.dialog_embeddings:
-                y = [self.vocab.talker1_bos_id] + y[:self.max_lengths-2] + [self.vocab.eos_id]
+                y = [self.vocab.talker1_bos_id] + y + [self.vocab.eos_id]
                 sentences.append(y)
             else:
-                y = [self.vocab.bos_id] + y[:self.max_lengths-2] + [self.vocab.eos_id]
+                y = [self.vocab.bos_id] + y + [self.vocab.eos_id]
                 if self.dialog_embeddings:
                     y = [[tok, self.vocab.sent_dialog_id] for tok in y]
                 sentences.append(y)
@@ -310,103 +323,3 @@ class TaskDataset(Dataset):
 
     def __len__(self):
         return len(self.data_list)
-
-class RandomReplaceDataset():
-    def __init__(self, path, tokenizer, fasttext_path, ratio, th=0.4, max_neighbors=5, data_type='persona'):
-        self.fasttext_model = fasttext.load_model(fasttext_path)
-        self.data_type = data_type
-        self.tokenizer = tokenizer
-        self.path = path
-        self.max_neighbors = max_neighbors
-        self.th = th
-        self.ratio = ratio
-
-    def build_dict(self, parsed_data):
-        token_set = set()
-        for persona in tqdm(parsed_data):
-            for k, v in persona.items():
-                for sentence in v:
-                    if isinstance(sentence, list):
-                        sentence = ' '.join(sentence)
-                    tokens = sentence.split(' ')
-                    for t in tokens:
-                        if t not in SPECIAL_TOKENS:
-                            if t[-1] in ['.', ',', '!', '?', ';']:
-                                token_set.add(t[:-1])
-                            elif t.find('.') != -1:
-                                ts = t.split('.')
-                                for tmp_t in ts:
-                                    token_set.add(tmp_t)
-                            else:
-                                token_set.add(t)
-        neighbor_dict = {}
-        for token in list(token_set):
-            neighbors = self.fasttext_model.get_nearest_neighbors(token)
-            neighbor_dict[token] = neighbors
-        return neighbor_dict
-
-    def add_tokenized_data(self, chat, dataset):
-        persona_info = [self.tokenizer.encode(self.tokenizer.tokenize(s)) for s in chat['persona_info']]
-        dialog = []
-        for i, replica in enumerate(chat['dialog'], 1):
-            dialog.append(self.tokenizer.encode(self.tokenizer.tokenize(replica)))
-            if not i % 2:
-                if chat['candidates']:
-                    candidates_ids = [self.tokenizer.encode(self.tokenizer.tokenize(c)) for c in chat['candidates'][(i - 1) // 2]]
-                    dataset.append((persona_info, dialog[:], candidates_ids))
-                else:
-                    dataset.append((persona_info, dialog[:], []))
-
-    def build_cache(self, cache_path):
-        if self.data_type == 'persona':
-            parsed_data = FacebookDataset.parse_data(self.path)
-        elif self.data_type == 'emoji':
-            parsed_data = FacebookDataset.parse_data_emoji(self.path)
-        neighbor_dict = self.build_dict(parsed_data)
-
-        dataset = []
-        for data in tqdm(parsed_data):
-            persona = data['persona_info']
-            dialog = data['dialog']
-            token_cnt = 0
-            persona_len = len(persona)
-            token_idx = []
-            token_list = []
-            for i, p in enumerate(persona):
-                tmp_tokens = p[:-1].split(' ')
-                tmp_tokens.append('.')
-                cur_tokens_list = []
-                for j, t in enumerate(tmp_tokens):
-                    cur_tokens_list.append(t)
-                    token_cnt += 1
-                    token_idx.append((i, j))
-                token_list.append(cur_tokens_list)
-            for i, d in enumerate(dialog):
-                if i % 2 != 0:
-                    continue
-                tmp_tokens = d.split(' ')
-                cur_tokens_list = []
-                for j, t in enumerate(tmp_tokens):
-                    cur_tokens_list.append(t)
-                    token_cnt += 1
-                    token_idx.append((i//2 + persona_len, j))
-                token_list.append(cur_tokens_list)
-            replace_num = math.ceil(token_cnt * self.ratio)
-            random.shuffle(token_idx)
-            idx = 0
-            while replace_num > 0:
-                replace_idx = token_idx[idx]
-                idx += 1
-                if idx >= len(token_idx):
-                    break
-                ori_token = token_list[replace_idx[0]][replace_idx[1]]
-                if neighbor_dict.__contains__(ori_token) and neighbor_dict[ori_token][0][0] > self.th:
-                    token_list[replace_idx[0]][replace_idx[1]] = neighbor_dict[ori_token][0][1]
-                    replace_num -= 1
-            self.add_tokenized_data(data, dataset)
-            replaced_texts = [' '.join(tokens) for tokens in token_list]
-            data['persona_info'] = replaced_texts[:persona_len]
-            for i in range(persona_len, len(replaced_texts)):
-                data['dialog'][(i - persona_len) * 2] = replaced_texts[i]
-            self.add_tokenized_data(data, dataset)
-        torch.save(dataset, cache_path)
